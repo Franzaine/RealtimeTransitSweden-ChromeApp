6b8fd2070d04fefd6b61bd39e4664de501ecbe64
Theo Franzén <franzen.theo@gmail.com>
2014-03-14 17:48:51 +0100

    Initial commit

---
+++ js/view_controller.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function() {
+
+	function toggleVisibilityFunction () {
+	
+		$("#departureContainer").toggleClass("visible"); 
+		$("#cardContainer").toggleClass("visible"); 
+		$("#backbutton").toggleClass("visible"); 
+		
+	}
+
+})
